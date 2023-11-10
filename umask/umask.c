23eca2a76953fd4420729fdb0127624b070d3b09
manticore-siva <siva007@bala2.0@gmail.com>
2023-11-10 16:14:17 +0530

    umask

---
+++ umask/umask.c
@@ -0,0 +1,42 @@
+/////////////////////////////////////////////////////////////////////////
+//                                                                     //
+//   Authore Name : Sivaprakasam.v                                     //
+//   Title        : Umask                                              //
+//   File Name    : umask.c                                            //
+//   Date         : 08/11/2023                                         //
+//                                                                     // 
+/////////////////////////////////////////////////////////////////////////
+
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+#define PERMISION S_IRWXU|S_IRWXG|S_IROTH
+
+int main(int arguc, char *arguv[])
+{
+    int r_mkdir;
+     mode_t mode, mkdir_ret, old_mask, new_mask;
+
+     mode = PERMISION;
+     new_mask = 027;
+
+     old_mask = umask(0);
+     printf("Old umask : %o\n",old_mask);
+
+     old_mask = umask(new_mask);
+     printf("after changed umask : %o\n",old_mask);
+     
+    // int mkdir(const char *pathname, mode_t mode);
+    r_mkdir = mkdir(arguv[1], new_mask);
+
+    if(r_mkdir == 0) 
+    {
+        perror("mkdir() : ");
+    }
+    else if (r_mkdir == -1)
+    {
+        perror("mkdir() : ");
+    }
+}
